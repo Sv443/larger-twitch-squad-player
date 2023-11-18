3b9fae12c2a1e44ce6d0ee962062713ad22148ad
Sven Fehler <38158426+Sv443@users.noreply.github.com>
2023-11-18 02:10:49 +0100

    fix: userutils v3 support

--- larger-twitch-squad-player.user.js
+++ larger-twitch-squad-player.user.js
@@ -5,11 +5,11 @@
 // @match       *://www.twitch.tv/*/squad
 // @grant       none
 // @license     MIT
-// @version     1.1
+// @version     1.2
 // @author      Sv443
 // @run-at      document-start
 // @icon        https://icons.duckduckgo.com/ip3/twitch.tv.ico
-// @require     https://greasyfork.org/scripts/472956-userutils/code/UserUtils.js
+// @require     https://update.greasyfork.org/scripts/472956/1279362/UserUtils.js
 // ==/UserScript==
 
 "use strict";
@@ -18,9 +18,10 @@ document.addEventListener("DOMContentLoaded", removeBar);
 
 function removeBar() {
   try {
-    UserUtils.initOnSelector();
+    const obs = new UserUtils.SelectorObserver(document.body);
+    obs.enable();
 
-    UserUtils.onSelector(".squad-stream-top-bar__container", {
+    obs.addListener(".squad-stream-top-bar__container", {
       listener: (bar) => {
         bar.remove();
         console.info("Removed title bar.\nUserscript by Sv443: https://github.com/Sv443/larger-twitch-squad-player");
